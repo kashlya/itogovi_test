6e41b685bace5e1f612fde22d44f819e9bd57818
kashlya <05lozaverlan@gmail.com>
2024-03-28 16:14:25 +0100

    2

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def chisla(pervoe, vtoroe, tretie, chetvertoe):
-    return max(pervoe, vtoroe, tretie, chetvertoe)
+    max_ch = pervoe
+    if vtoroe > max_ch:
+        max_ch = vtoroe
+    if tretie > max_ch:
+        max_ch = tretie
+    if chetvertoe > max_ch:
+        max_ch = chetvertoe
+    return max_ch
 
 
 print(chisla(1, 5, 3, 2))
