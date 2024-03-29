d3a5c601f90107dc61055b8b30aa05447f50d316
kashlya <05lozaverlan@gmail.com>
2024-03-29 17:31:05 +0100

    5

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-def prostoe_li_chislo(chislo):
-    if chislo % 2 == 0:
-        return "false"
-    if chislo <= 1:
-        return "false"
-    if chislo == 2:
-        return "true"
-    for i in range(3, int(chislo ** 0.5) + 1, 2):
-        if chislo % i == 0:
-            return False
-    return True
+def lucky_chislo(chislo):
+    b = []
+    while chislo > 0:
+        b.append(chislo % 10)
+        chislo = chislo // 10
+    b = b[::-1]
+    if b[0] + b[1] + b[2] == b[3] + b[4] + b[5]:
+        print("true")
+    else:
+        print("false")
 
 
-print(prostoe_li_chislo(41))
+lucky_chislo(612711)
